6b4faea49855d2ce4b71522fbfdb5b451680da56
Dovydas <dovydas.skackovas@gmail.com>
2025-11-10 20:32:26 +0200

    changed end date for predictions

--- src/lib/utils/constants.ts
+++ src/lib/utils/constants.ts
@@ -1,7 +1,7 @@
 export const CURRENT_SEASON = "2024-25";
 
-// Oct 22, 2025, 2:30 AM EET = Oct 22, 2025, 12:30 AM UTC
-export const SEASON_START_DATE = new Date("2025-11-01T00:30:00.000Z");
+// Nov 22, 2025, 2:30 AM EET = Nov 22, 2025, 12:30 AM UTC
+export const SEASON_START_DATE = new Date("2025-11-12T00:30:00.000Z");
 
 export const TIMEZONE = "Europe/Vilnius"; // EET
 
